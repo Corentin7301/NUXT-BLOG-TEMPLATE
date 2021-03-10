c129e82f296b41f0abab76c4e1e857f83d723d74
corentinperroux <corentin7301@gmail.com>
2021-03-10 10:46:17 +0100

    add button composent

---
+++ components/Button.vue
@@ -0,0 +1,37 @@
+<template>
+  <button :type="type"><slot/></button>
+</template>
+
+<script>
+export default {
+props: {
+    type: {
+        type: String,
+        default: ""
+    },
+    url: {
+        type: String,
+        default: ""
+    }
+},
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+    padding: 10px 0;
+    width: 200px;
+    background-color: $green;
+    color: $white;
+    transition: 0.3s;
+    cursor: pointer;
+    border: none;
+    border-radius: 5px;
+
+    &:hover {
+    transition: 0.2s;
+    background-color: $white;
+    color: $green;
+    }
+}
+</style>
