76c02ba4f2d859e9444b019d579d00a86eae986e
corentinperroux <corentin7301@gmail.com>
2021-03-11 15:24:22 +0100

    fix bug last page variabilize with json datas

--- utils/getContent.js
+++ utils/getContent.js
@@ -1,35 +1,26 @@
+import global from "@/blog.config.json"
+
 export default async ($content, params, error) => {
   const currentPage = parseInt(params.page);
 
-  const perPage = 9;
+  const perPage = global.blog.articleCountPerPage;
 
   const allArticles = await $content("articles").only(['title', 'slug', 'description', 'createdAt', 'urlImage']).fetch();
-
-  const totalArticles = allArticles.length;
-
-  // use Math.ceil to round up to the nearest whole number
-  const lastPage = Math.ceil(totalArticles / perPage);
-
-  // use the % (modulus) operator to get a whole remainder
-  const lastPageCount = totalArticles % perPage;
 
   const skipNumber = () => {
     if (currentPage === 1) {
       return 0;
     }
-    if (currentPage === lastPage) {
-      return totalArticles - lastPageCount;
-    }
+
     return (currentPage - 1) * perPage;
   };
-
+  
   const paginatedArticles = await $content("articles")
     .only(['title', 'slug', 'description', 'createdAt', 'urlImage'])
     .sortBy("createdAt", "desc")
     .limit(perPage)
     .skip(skipNumber())
     .fetch();
-
   if (currentPage === 0 || !paginatedArticles.length) {
     return error({
       statusCode: 404,
